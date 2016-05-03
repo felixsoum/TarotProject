312bd2f003cf0d1e44a7451e54a7bd56e65fbdb2
felixsoum <felix@felixsoum.com>
2016-05-03 00:34:31 -0400

    Add table and cards

---
+++ Assets/Scripts/CardController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class CardController : MonoBehaviour
+{
+    Rigidbody myRigidbody;
+    bool isActive;
+    const float ScreenToWorldDelta = 0.0025f;
+
+    void Awake()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void StartUse()
+    {
+        isActive = true;
+        myRigidbody.useGravity = false;
+        Vector3 pos = transform.position;
+        pos.y = 1.1f;
+        transform.position = pos;
+    }
+
+    public void FinishUse()
+    {
+        isActive = false;
+        myRigidbody.useGravity = true;
+    }
+
+    public void MoveCard(Vector3 deltaPos)
+    {
+        Vector3 pos = transform.position;
+        pos.x += deltaPos.x * ScreenToWorldDelta;
+        pos.z += deltaPos.y * ScreenToWorldDelta;
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/InteractionController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class InteractionController : MonoBehaviour
+{
+    public LayerMask interactableLayer;
+    CardController card;
+    Vector3 previousMousePos;
+    bool isInteracting;
+
+	void Update()
+    {
+	    if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitHinfo;
+            if (Physics.Raycast(ray, out hitHinfo, 5f, interactableLayer))
+            {
+                isInteracting = true;
+                previousMousePos = Input.mousePosition;
+                card = hitHinfo.transform.gameObject.GetComponent<CardController>();
+                card.StartUse();
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isInteracting = false;
+            card.FinishUse();
+        }
+        else if (isInteracting && Input.GetMouseButton(0))
+        {
+            Vector3 deltaPos = Input.mousePosition - previousMousePos;
+            previousMousePos = Input.mousePosition;
+            card.MoveCard(deltaPos);
+        }
+	}
+}
