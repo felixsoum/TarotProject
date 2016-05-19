ea715686c825a263504e9e09e41ce283a88201fa
felixsoum <felix@felixsoum.com>
2016-05-18 21:19:26 -0400

    Add knife controller

---
+++ Assets/Scripts/KnifeController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class KnifeController : InteractableController
+{
+    public float throwForce = 10f;
+    Rigidbody myRigidbody;
+    const float ScreenToWorldDelta = 0.0025f;
+    bool isThrown;
+
+    void Awake()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (isThrown)
+        {
+            return;
+        }
+    }
+
+    public override void StartUse()
+    {
+        myRigidbody.useGravity = false;
+        myRigidbody.isKinematic = true;
+        Vector3 pos = transform.position;
+        pos.y = 1.1f;
+        transform.position = pos;
+        transform.eulerAngles = new Vector3(270, 270, 0);
+    }
+
+    public override void FinishUse()
+    {
+        myRigidbody.useGravity = true;
+        myRigidbody.isKinematic = false;
+    }
+
+    public override void StartAltUse()
+    {
+        isThrown = true;
+        FinishUse();
+        myRigidbody.AddForce(throwForce * Vector3.forward, ForceMode.Impulse);
+    }
+
+    public override void UpdatePos(Vector3 deltaPos)
+    {
+        if (isThrown)
+        {
+            return;
+        }
+        Vector3 pos = transform.position;
+        pos.x += deltaPos.x * ScreenToWorldDelta;
+        pos.z += deltaPos.y * ScreenToWorldDelta;
+        transform.position = pos;
+    }
+}
